5308296397dea184779dc5c81a2dba9c0959ad07
Duy <hi.am.not.me@gmail.com>
2019-04-05 12:59:02 +0700

    Add Main activity

--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.bkteam.ohmychat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Mapping();
     }
+
+    private void Mapping() {
+        btnLogin = findViewById(R.id.btnLogin);
+        username = findViewById(R.id.edtUsername);
+        password = findViewById(R.id.edtPassword);
+
+        //TODO: Change later
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("username", username.getText().toString());
+                intent.putExtra("password", password.getText().toString());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
 }
--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/MainActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/MainActivity.java
@@ -1,13 +1,62 @@
 package com.bkteam.ohmychat;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout topToolbar;
+    private ActionBar bottomToolbar;
+    private TextView test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_chat:
+                        break;
+                    case R.id.navigation_profile:
+                        break;
+                    case R.id.navigation_settings:
+                        break;
+                }
+            }
+        });
+
+        // For testing
+            test = findViewById(R.id.test);
+        //
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+
+        }
+        else {
+            test.setText("Username: " + extras.get("username") + "\n" + "Password: " + extras.get("password"));
+        }
+
     }
+
+
 }
