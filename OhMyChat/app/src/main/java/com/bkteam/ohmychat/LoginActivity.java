8c5e8f3f1c2d755a5f3267d2a52b8ac23e919430
ThanhTung <thanhtungbs00@gmail.com>
2019-04-07 05:59:25 +0700

    Add Register View - Add Bar layout.

--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
     EditText username;
     EditText password;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         username = findViewById(R.id.edtUsername);
         password = findViewById(R.id.edtPassword);
+        register = findViewById(R.id.tvSignUp);
 
         //TODO: Change later
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +35,15 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.putExtra("username", username.getText().toString());
                 intent.putExtra("password", password.getText().toString());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
