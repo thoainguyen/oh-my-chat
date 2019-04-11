b8a0850377d3b42744deaf21c7810c9fac59c569
Thoai. Nguyen Huynh <thoainh@vng.com.vn>
2019-04-11 23:31:14 +0700

    add chat group

--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/GroupChatActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/GroupChatActivity.java
@@ -1,6 +1,7 @@
 package com.bkteam.ohmychat;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +24,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 
 public class GroupChatActivity extends AppCompatActivity {
@@ -61,7 +64,57 @@ public class GroupChatActivity extends AppCompatActivity {
                 userMessageInput.setText("");
             }
         });
+
+
     }
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        groupNameRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    DisplayMessages(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void DisplayMessages(DataSnapshot dataSnapshot) {
+        Iterator iterator = dataSnapshot.getChildren().iterator();
+
+        while(iterator.hasNext()){
+            String chatDate = (String)((DataSnapshot)iterator.next()).getValue();
+            String chatMessage = (String)((DataSnapshot)iterator.next()).getValue();
+            String chatName = (String)((DataSnapshot)iterator.next()).getValue();
+            String chatTime = (String)((DataSnapshot)iterator.next()).getValue();
+
+            displayTextMessages.append(chatName + " :\n" + chatMessage + "\n" +
+                    chatTime + "    " + chatDate + "\n\n\n");
+        }
+    }
+
 
     private void SaveMessageInfoToDatabase() {
         String message = userMessageInput.getText().toString();
