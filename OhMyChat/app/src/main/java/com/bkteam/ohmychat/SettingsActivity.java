f8e3bbc42c1f2fceeedf5e9669ced83ea4a93fb3
Thoai. Nguyen Huynh <thoainh@vng.com.vn>
2019-04-11 17:02:30 +0700

    retrive data from firebase

--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/SettingsActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/SettingsActivity.java
@@ -13,8 +13,11 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -40,10 +43,49 @@ public class SettingsActivity extends AppCompatActivity {
         currentUserID = mAuth.getCurrentUser().getUid();
         rootRef = FirebaseDatabase.getInstance().getReference();
 
+        userName.setVisibility(View.INVISIBLE);
+
+
         updateAccountSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 UpdateSettings();
+            }
+
+        });
+
+        RetrieveUserInfo();
+    }
+
+    private void RetrieveUserInfo() {
+        rootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.exists()) && (dataSnapshot.hasChild("name")) &&
+                        (dataSnapshot.hasChild("image"))){
+                    String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                    String retrieveUserStatus = dataSnapshot.child("status").getValue().toString();
+                    String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                    userName.setText(retrieveUserName);
+                    userStatus.setText(retrieveUserStatus);
+                }
+                else if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name"))){
+                        String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                        String retrieveUserStatus = dataSnapshot.child("status").getValue().toString();
+
+                        userName.setText(retrieveUserName);
+                        userStatus.setText(retrieveUserStatus);
+                }
+                else {
+                    userName.setVisibility(View.VISIBLE);
+                    Toast.makeText(SettingsActivity.this, "Please set & update profile...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
