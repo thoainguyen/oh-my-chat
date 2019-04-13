d5cc0596f8bfa9f7d34604b77894eaec06fe2a46
Thoai. Nguyen Huynh <thoainh@vng.com.vn>
2019-04-13 15:26:17 +0700

    phone number auth -> disable in free firebase version

--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/LoginActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -53,10 +52,12 @@ public class LoginActivity extends  AppCompatActivity{
 
 
         });
+
+
         phoneLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent phoneLoginIntent = new Intent(LoginActivity.this, PhActivity.class);
+                Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
                 startActivity(phoneLoginIntent);
             }
         });
