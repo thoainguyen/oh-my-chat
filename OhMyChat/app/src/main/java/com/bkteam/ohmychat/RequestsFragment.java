bf6716c2cacdf2173145f0e8fb2840a412997e7f
Thoai. Nguyen Huynh <thoainh@vng.com.vn>
2019-04-24 21:00:31 +0700

    add request tab

---
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/RequestsFragment.java
@@ -0,0 +1,140 @@
+package com.bkteam.ohmychat;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RequestsFragment extends Fragment {
+
+    private View requestsFragmentView;
+    private RecyclerView mRequestsList;
+    private DatabaseReference chatRequestsRef, userRef;
+    private FirebaseAuth mAuth;
+    private String currentUserId;
+
+    public RequestsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        chatRequestsRef = FirebaseDatabase.getInstance().getReference().child("Chat Requests");
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        requestsFragmentView = inflater.inflate(R.layout.fragment_requests, container, false);
+        mRequestsList = (RecyclerView)requestsFragmentView.findViewById(R.id.chat_requests_list);
+        mRequestsList.setLayoutManager(new LinearLayoutManager(getContext()));
+        return requestsFragmentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(chatRequestsRef.child(currentUserId), Contacts.class)
+                .build();
+        final FirebaseRecyclerAdapter<Contacts, RequestsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, RequestsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull final RequestsViewHolder holder, int position, @NonNull Contacts model) {
+                        holder.itemView.findViewById(R.id.requests_accept_btn).setVisibility(View.VISIBLE);
+                        holder.itemView.findViewById(R.id.requests_cancel_btn).setVisibility(View.VISIBLE);
+                        final String listUserId = getRef(position).getKey();
+                        DatabaseReference getTypeRef = getRef(position).child("requestType").getRef();
+                        getTypeRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.exists()){
+                                    String type = dataSnapshot.getValue().toString();
+                                    if(type.equals("received")){
+                                        userRef.child(listUserId).addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                if(dataSnapshot.hasChild("image")){
+                                                    final String requestUserName = dataSnapshot.child("name").getValue().toString();
+                                                    final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+                                                    final String requestProfileImage = dataSnapshot.child("image").getValue().toString();
+                                                    holder.userName.setText(requestUserName);
+                                                    holder.userStatus.setText(requestUserStatus);
+                                                    Picasso.get().load(requestProfileImage).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+                                                }
+                                                else{
+                                                    final String requestUserName = dataSnapshot.child("name").getValue().toString();
+                                                    final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+                                                    holder.userName.setText(requestUserName);
+                                                    holder.userStatus.setText(requestUserStatus);
+
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                    @NonNull
+                    @Override
+                    public RequestsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
+                        RequestsViewHolder holder = new RequestsViewHolder(view);
+                        return holder;
+                    }
+                };
+        mRequestsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class RequestsViewHolder extends  RecyclerView.ViewHolder{
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+        Button acceptButton, cancelButton;
+        public RequestsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userName = itemView.findViewById(R.id.users_profile_name);
+            userStatus = itemView.findViewById(R.id.users_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+            acceptButton = (Button)itemView.findViewById(R.id.requests_accept_btn);
+            cancelButton = (Button)itemView.findViewById(R.id.requests_cancel_btn);
+
+        }
+    }
+}
--- OhMyChat/app/src/main/java/com/bkteam/ohmychat/TabsAccessorAdapter.java
+++ OhMyChat/app/src/main/java/com/bkteam/ohmychat/TabsAccessorAdapter.java
@@ -24,6 +24,9 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
             case 2:
                 ContactsFragment contactsFragment = new ContactsFragment();
                 return contactsFragment;
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
             default:
                 return null;
         }
@@ -31,7 +34,7 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return 4;
     }
 
     @Nullable
@@ -44,6 +47,8 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
                 return  "Groups";
             case 2:
                 return "Contacts";
+            case 3:
+                return "Requests";
             default:
                 return null;
         }
